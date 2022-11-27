105e4378baa6ef6ac0d3b85a868c017b865aeb9a
IvanSB0066 <119069332+IvanSB0066@users.noreply.github.com>
2022-11-27 17:33:42 +0100

    CONSULTAS SQL

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -30,7 +30,11 @@ b. Obtener el nombre y apellidos del cartero que ha realizado el reparto del paq
 con menor peso, así como la matricula del coche con el que hizo dicho reparto, y el peso 
 de dicho paquete.
 */
-
+SELECT cartero.nombre, cartero.apellido, reparto.matricula FROM cartero
+INNER JOIN reparto ON cartero.dni = reparto.dni_cartero
+INNER JOIN paquete ON reparto.id_reparto = paquete.id_reparto
+WHERE paquete.peso <= ALL ( SELECT paquete.peso FROM paquete)
+;
 /*
 c. Obtener el nombre, apellidos y dirección completa de los usuarios que han enviado 
 cartas normales y certificadas (devolver el tipo de carta también) dentro del área de envío 
@@ -63,7 +67,9 @@ FROM uidentificado
 d. Obtener el numero total de repartos realizados por cada cartero en las diferentes 
 oficinas ordenado de mayor a menor.
 */
-
+SELECT DISTINCT COUNT(*)
+FROM reparto
+ORDERED BY DESC
 /*
 e. Obtener el peso total de los paquetes y el nivel mayor de urgencia de las cartas certificadas 
 que ha llevado los coches de la oficina “OF-MAD-O1” en los repartos que se les han asignado en los 
