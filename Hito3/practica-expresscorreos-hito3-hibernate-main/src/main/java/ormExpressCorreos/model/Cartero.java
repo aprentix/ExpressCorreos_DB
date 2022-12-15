7808a1adbf298a35b6990b9b6ae744e538ee4136
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 17:26:50 +0100

    Creado Cartero.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Cartero.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Cartero.java
@@ -1,4 +1,40 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Cartero")
 public class Cartero {
+    @Id
+    @GeneratedValue
+    @Column(name = "DNI")
+    private String DNI;
+
+    @Column(name = "Nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "Apellidos",  nullable = false)
+    private String apellidos;
+
+    public Cartero(String DNI, String nombre, String apellidos) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public Cartero() {
+
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
 }
