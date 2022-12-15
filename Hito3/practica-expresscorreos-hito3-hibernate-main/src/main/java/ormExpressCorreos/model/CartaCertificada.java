a17a1d38941d14d5b05c057aa14091a52863a107
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 11:32:01 +0100

    Creado CartaCertificada.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/CartaCertificada.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/CartaCertificada.java
@@ -1,6 +1,27 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Carta Certificada")
 public class CartaCertificada {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
     private int id;
+
+    @Column(name = "Nivel de Urgencia", nullable = false)
     private String nivelUrgencia;
+
+    @ManyToOne
+    @JoinColumn(name = "UIrecibe")
+    private UsuarioIdentificado UIrecibe;
+
+    @ManyToOne
+    @JoinColumn (name = "UIenvia")
+    private UsuarioIdentificado UIenvia;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "Reparto")
+    private Reparto reparto;
 }
