5d3501a1dbc0f22148f407727b94f8069316b04c
fatimaakbarova <95278838+fatimaakbarova@users.noreply.github.com>
2022-12-15 18:49:04 +0100

    Update Direccion.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Direccion.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Direccion.java
@@ -1,15 +1,44 @@
 package ormExpressCorreos.model;
-
+import javax.persistence.*;
 // @TODO completar las anotaciones de la clase
+@Entity
+@Table(name = "Direccion")
 public class Direccion {
-
     // @TODO completar las anotaciones de todos los atributos
 
-    public Direccion(Integer numero, Integer piso, String letra, String portal, ...) {  // @TODO: completar
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
+    private Integer id;
+
+    @Column(name = "Numero")
+    private Integer numero;
+
+    @Column(name = "Piso")
+    private String piso;
+
+    @Column(name = "Letra")
+    private String letra;
+
+    @Column(name = "Portal")
+    private Integer portal;
+
+    @ManyToOne
+    @JoinColumn(name="Id_calle")
+    private Calle id_calle;
+
+
+    public Direccion(Integer id, Integer numero, Integer piso, String letra, String portal, Calle id_calle, Integer id1, Integer numero1, String piso1, String letra1, Integer portal1, Calle id_calle1) {  // @TODO: completar
         // @TODO completar el constructor de la clase.
         //  Para ello son necesarios dos enteros con el número y el piso, y dos strings
         // con la letra y el portal
         //  Cree e inicialice el resto de atributos a los valores que considere oportunos
+        this.id = id1;
+        this.numero = numero1;
+        this.piso = piso1;
+        this.letra = letra1;
+        this.portal = portal1;
+        this.id_calle = id_calle1;
     }
 
     public Integer getNumero() {
