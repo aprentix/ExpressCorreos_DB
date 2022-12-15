bc23d933fefc86d730dc2625f9db57b57669e101
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 10:38:06 +0100

    Todas las clases de las entidades, falta rellenarlas

---
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/CartaCertificada.java
@@ -0,0 +1,6 @@
+package ormExpressCorreos.model;
+
+public class CartaCertificada {
+    private int id;
+    private String nivelUrgencia;
+}
---
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Turno.java
@@ -0,0 +1,4 @@
+package ormExpressCorreos.model;
+
+public class Turno {
+}
