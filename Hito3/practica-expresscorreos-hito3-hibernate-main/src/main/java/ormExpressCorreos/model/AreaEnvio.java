1cefd323c77e21cc2101d1dd2042cca2531fc966
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 11:59:09 +0100

    Creado AreaEnvio.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/AreaEnvio.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/AreaEnvio.java
@@ -1,4 +1,41 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Area Envio")
 public class AreaEnvio {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
+    private int id;
+
+    @Column(name = "Nombre")
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "Oficina")
+    private Oficina oficina;
+
+    public AreaEnvio(int id, String nombre, Oficina oficina) {
+        this.id = id;
+        this.nombre = nombre;
+        this.oficina = oficina;
+    }
+
+    public AreaEnvio() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Oficina getOficina() {
+        return oficina;
+    }
 }
