89691a8e31c69524a13d30b8a531619240d7c389
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 17:22:48 +0100

    Creado Turno.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Turno.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Turno.java
@@ -1,4 +1,32 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Turno")
 public class Turno {
+    @Id
+    @GeneratedValue
+    @Column(name = "Jornada")
+    private String jornada;
+
+    @Column(name = "Horario", nullable = false)
+    private String Horario;
+
+    public Turno(String jornada, String horario) {
+        this.jornada = jornada;
+        Horario = horario;
+    }
+
+    public Turno() {
+
+    }
+
+    public String getJornada() {
+        return jornada;
+    }
+
+    public String getHorario() {
+        return Horario;
+    }
 }
