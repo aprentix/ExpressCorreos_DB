378044874f78fcf1b010f33e12e6ea749c879bf8
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 12:03:04 +0100

    Creado Calle.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Calle.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Calle.java
@@ -1,4 +1,41 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Calle")
 public class Calle {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
+    private int id;
+
+    @Column(name = "Nombre", nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "Municipio")
+    private Municipio municipio;
+
+    public Calle(int id, String nombre, Municipio municipio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.municipio = municipio;
+    }
+
+    public Calle() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Municipio getMunicipio() {
+        return municipio;
+    }
 }
