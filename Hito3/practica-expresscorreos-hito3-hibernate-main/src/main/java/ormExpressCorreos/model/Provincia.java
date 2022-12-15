47289a7994db8679076eb3c1d1cfeb5cc8f7a08e
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 12:22:19 +0100

    Creado Provincia.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Provincia.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Provincia.java
@@ -1,4 +1,27 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Provincia")
 public class Provincia {
+    @Id
+    @Column(name = "Nombre")
+    private String nombre;
+
+    @OneToMany(mappedBy = "Provincia")
+    private Set<Municipio> municipios;
+
+    public Provincia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Provincia() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
