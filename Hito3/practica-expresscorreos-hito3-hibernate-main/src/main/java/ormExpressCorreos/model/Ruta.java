682a2b704e4ebe2950c66715263afcb062c49d9d
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 17:19:26 +0100

    Creado Ruta.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Ruta.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Ruta.java
@@ -1,4 +1,24 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Ruta")
 public class Ruta {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private int matricula;
+
+    public Ruta(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Ruta() {
+
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
 }
