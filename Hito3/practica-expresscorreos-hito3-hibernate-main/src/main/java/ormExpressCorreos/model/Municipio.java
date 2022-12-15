02381f232ae4a9efd854e9a2a08147bdd534f925
Damian <damian.sanchez@alumnos.upm.es>
2022-12-15 12:18:18 +0100

    Creado Municipio.java

--- Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Municipio.java
+++ Hito3/practica-expresscorreos-hito3-hibernate-main/src/main/java/ormExpressCorreos/model/Municipio.java
@@ -1,4 +1,45 @@
 package ormExpressCorreos.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Municipio")
 public class Municipio {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
+    private int id;
+
+    @Column(name = "Nombre", nullable = false, unique = true)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "Provincia")
+    private Provincia provincia;
+
+    @OneToMany(mappedBy = "Municipio")
+    private Set<Calle> calles;
+
+    public Municipio(int id, String nombre, Provincia provincia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
+
+    public Municipio() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Provincia getProvincia() {
+        return provincia;
+    }
 }
